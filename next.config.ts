416f1c72b4840b711fd979dc9570cc7047a8c6ed
90945 <909458209@qq.com>
2025-12-28 18:44:06 +0800

    next配置改为output: 'standalone',

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   basePath: '/next',
+  output: 'standalone',
+  // 使用nginx的br压缩
+  compress: false,
 };
 
 export default nextConfig;
