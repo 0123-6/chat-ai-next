fcf86d6c9def16ba743643652a940711ea7d0cdd
90945 <909458209@qq.com>
2026-02-05 07:10:14 +0800

    优化api.ts,支持不展示错误信息

--- src/util/api.ts
+++ src/util/api.ts
@@ -69,6 +69,8 @@ export interface IBaseFetch {
 
   // 返回的是不是文件流
   isFile?: boolean,
+  // 展示错误提示
+  showErrorMessage?: boolean,
 }
 
 export interface IResponseData {
@@ -103,6 +105,7 @@ export async function baseFetch(props: IBaseFetch)
     signal,
     isFormData = false,
     isFile = false,
+    showErrorMessage = true,
   } = props
   let {
     url,
@@ -181,7 +184,9 @@ export async function baseFetch(props: IBaseFetch)
 
     // 接口无效或服务器错误
     if (!response.ok) {
-      errorMessage('api请求失败：' + response.status + ' 接口无效或服务器错误')
+      if (showErrorMessage) {
+        errorMessage('api请求失败：' + response.status + ' 接口无效或服务器错误')
+      }
       return {
         isOk: false,
         reason: 'api请求失败：' + response.status + ' 接口无效或服务器错误',
@@ -195,7 +200,9 @@ export async function baseFetch(props: IBaseFetch)
       const responseData: IResponseData = await response.json()
       // 正常情况为fetchApiResponseCodeMap.success数组的一个
       if (!projectConfig.fetchApiResponseCodeMap.success.includes(responseData.code)) {
-        errorMessage(responseData.msg || responseData.message || `${url}接口异常`)
+        if (showErrorMessage) {
+          errorMessage(responseData.msg || responseData.message || `${url}接口异常`)
+        }
         // 账号超时，需要重新登录
         if (projectConfig.fetchApiResponseCodeMap.notLogin.includes(responseData.code)) {
           goLoginPage()
@@ -206,7 +213,9 @@ export async function baseFetch(props: IBaseFetch)
         }
       } else if (isFile) {
         // 文件类型得到json表示错误
-        errorMessage('文件下载失败: ' + responseData?.msg || responseData?.message || '')
+        if (showErrorMessage) {
+          errorMessage('文件下载失败: ' + responseData?.msg || responseData?.message || '')
+        }
         return {
           isOk: false,
           reason: '文件下载失败: ' + responseData?.msg || responseData?.message || '',
@@ -229,7 +238,9 @@ export async function baseFetch(props: IBaseFetch)
       }
       const responseBlob = await response.blob()
       if (responseBlob.size === 0) {
-        errorMessage('文件为空文件,无法下载')
+        if (showErrorMessage) {
+          errorMessage('文件为空文件,无法下载')
+        }
         return {
           isOk: false,
           reason: '文件为空文件,无法下载',
@@ -243,7 +254,9 @@ export async function baseFetch(props: IBaseFetch)
           successMessage('文件下载成功')
         },
         callbackError: text => {
-          errorMessage(text)
+          if (showErrorMessage) {
+            errorMessage(text)
+          }
         },
       })
       return {
@@ -258,7 +271,9 @@ export async function baseFetch(props: IBaseFetch)
         reason: 'AbortError',
       }
     } else {
-      errorMessage('api请求失败：网络错误')
+      if (showErrorMessage) {
+        errorMessage('api请求失败：网络错误')
+      }
       return {
         isOk: false,
         reason: 'api请求失败：网络错误',
